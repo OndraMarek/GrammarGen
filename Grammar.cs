cc4c5aedf363bac8da3302eb098854328e41ecde
OndraMarek <czondrejmarek@gmail.com>
2025-03-17 20:20:07 +0100

    implemented GenerateRandomWord and GenerateRandomWords methods for ContextFreeGrammar

--- Grammar.cs
+++ Grammar.cs
@@ -1,8 +1,14 @@
-﻿abstract class Grammar
+﻿using System;
+
+abstract class Grammar
 {
-    public HashSet<string> NonTerminals { get; set; } = new();
-    public HashSet<string> Terminals { get; set; } = new();
+    public HashSet<string> NonTerminals { get; set; } = [];
+    public HashSet<string> Terminals { get; set; } = [];
     public string StartSymbol { get; set; } = "";
+    public string Word { get; set; } = "";
+    public HashSet<string> Words { get; set; } = [];
+
+    public static readonly Random random = new Random();
 
     public Grammar(string filePath)
     {
@@ -20,11 +26,16 @@
         NonTerminals = new HashSet<string>(lines[1].Split(';', StringSplitOptions.RemoveEmptyEntries));
         Terminals = new HashSet<string>(lines[2].Split(';', StringSplitOptions.RemoveEmptyEntries));
         StartSymbol = lines[4].Trim();
+        Word = StartSymbol;
 
         ParseRules(lines[3]);
     }
 
     protected abstract void ParseRules(string rulesLine);
+
+    protected abstract string GenerateRandomWord(string symbol);
+
+    protected abstract HashSet<string> GenerateRandomWords(string symbol, int count);
 
     public abstract void PrintGrammar();
 }
@@ -55,11 +66,44 @@ class ContextFreeGrammar : Grammar
         }
     }
 
-    public string GenerateRandomString()
+    protected override string GenerateRandomWord(string symbol)
     {
-        //TODO: Implementace generování náhodného řetězce
-        return "";
+        if (!NonTerminals.Contains(symbol))
+        {
+            return symbol;
+        }
+
+        if (Rules.ContainsKey(symbol))
+        {
+            List<string> rules = Rules[symbol];
+            string rule = rules[random.Next(rules.Count)];
+
+            Word = "";
+            for (int i = 0; i < rule.Length; i++)
+            {
+                string currentSymbol = rule[i].ToString();
+                Word += GenerateRandomWord(currentSymbol);
+            }
+            return Word;
+        }
+
+        return symbol;
     }
+
+    protected override HashSet<string> GenerateRandomWords(string symbol, int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            do
+            {
+                Word = GenerateRandomWord(symbol);
+            } while (Words.Contains(Word));
+
+            Words.Add(Word);
+        }
+        return Words;
+    }
+
     public override void PrintGrammar()
     {
         Console.WriteLine("Bezkontextová gramatika");
@@ -71,7 +115,12 @@ class ContextFreeGrammar : Grammar
             Console.WriteLine($"  {rule.Key} -> {string.Join(" | ", rule.Value)}");
         }
         Console.WriteLine("Startovací symbol: " + StartSymbol);
-        Console.WriteLine("Vygenerovaný řetězec: " + GenerateRandomString());
+        Words = GenerateRandomWords(StartSymbol, 10);
+        Console.WriteLine("Vygenerované řetězce:");
+        foreach (string word in Words)
+        {
+            Console.WriteLine("  " + word);
+        }
     }
 }
 
@@ -110,10 +159,16 @@ class MatrixGrammar : Grammar
 
         Matrices = matrices;
     }
-    public string GenerateRandomString()
+    protected override string GenerateRandomWord(string symbol)
     {
         //TODO: Implementace generování náhodného řetězce
         return "";
+    }
+
+    protected override HashSet<string> GenerateRandomWords(string symbol, int count)
+    {
+        //TODO: Implementace generování count náhodných řetězců
+        return [];
     }
 
     public override void PrintGrammar()
@@ -134,6 +189,13 @@ class MatrixGrammar : Grammar
             }
             index++;
         }
-        Console.WriteLine("Vygenerovaný řetězec: " + GenerateRandomString());
+        Word = GenerateRandomWord(StartSymbol);
+        Console.WriteLine("Uložené slovo: " + Word);
+        Words = GenerateRandomWords(StartSymbol, 10);
+        Console.WriteLine("Vygenerované řetězce:");
+        foreach (string word in Words)
+        {
+            Console.WriteLine("  " + word);
+        }
     }
 }
